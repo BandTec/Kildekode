d8c8c6553874ed76bfc9e326cbb57df915e11092
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-04-05 18:17:17 -0300

    git

---
+++ public/swing/frontSwing.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Exercicios{
+
+public static void main(String[] args){
+    
+    JFrame janela = new JFrame();
+
+    janela.setSize(800, 600);        
+    janela.setTitle("Tela usando JFrame");
+    janela.setLocationRelativeTo(null); // para aparecer no meio da tela
+    janela.setVisible(true);
+
+}
+
+}

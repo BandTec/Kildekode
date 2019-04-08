04972554284715d39ba8b6aeeb62441669b9a65d
Aluno <Aluno@Digital.net>
2019-04-08 20:26:16 -0300

    are

--- Maven/kildeKode/src/main/java/com/mycompany/kildekode/KildeKode.java
+++ Maven/kildeKode/src/main/java/com/mycompany/kildekode/KildeKode.java
@@ -1,44 +1,25 @@
 
 package com.mycompany.kildekode;
 
-import java.io.File; 
-import java.io.IOException; 
-import java.nio.file.FileStore; 
-import java.nio.file.Files; 
-import java.util.Arrays; 
- 
- 
-import org.slf4j.Logger; 
-import org.slf4j.LoggerFactory; 
- 
-import com.sun.jna.Platform; 
 import javax.swing.JOptionPane;
  
 import oshi.hardware.HardwareAbstractionLayer; 
-import oshi.hardware.GlobalMemory; 
-import oshi.hardware.PowerSource; 
-import oshi.hardware.CentralProcessor; 
 import oshi.hardware.ComputerSystem;
-import oshi.software.os.OSFileStore; 
-import oshi.software.os.OperatingSystem; 
-import oshi.software.os.OperatingSystemVersion; 
-import oshi.util.FormatUtil; 
-import oshi.util.Util; 
 /**
  *
  * @author lucas
  */
 public class KildeKode {
     
-    public void getProcessador(){
-       
-    }
-    
     public static void main(String[] args) {
         oshi.SystemInfo si = new oshi.SystemInfo(); 
         HardwareAbstractionLayer hal = si.getHardware();
+        hal.getComputerSystem();
+        System.out.println("teste: "+hal.getComputerSystem());
+        JOptionPane.showMessageDialog(null,"proc: "+hal.getDiskStores());
         ComputerSystem os = hal.getComputerSystem();
         oshi.hardware.Baseboard bs = os.getBaseboard();
-        JOptionPane.showMessageDialog(null,bs.getSerialNumber());
+        JOptionPane.showMessageDialog(null,"Serial: " + bs.getSerialNumber());
     }
+    
 }
